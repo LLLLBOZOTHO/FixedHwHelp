d4d5105480dd9b28cbfd1b501188569bcf70a751
You Thought <56780234+itamar1238@users.noreply.github.com>
2022-07-10 07:10:11 +0000

    stuff

--- static/keyp.js
+++ static/keyp.js
@@ -8,6 +8,11 @@ $(document).keydown(function(e) {
         if (getCookie('panic') == null) {
           document.cookie = "panic=classroom.google.com;"
         }
+        // if (getCookie('panic') == null) {
+        //    window.open('https://classroom.google.com')
+        //    console.log('Cookie cant be set!')
+        //    return
+        // }
         window.open("https://" + getCookie('panic'))
     }
 });
